5a88f8e02eb46546853b472ca08b0256732e3f7a
Farid-19 <faridamali93@gmail.com>
2014-09-25 10:46:12 +0200

    Engin's code for the client

--- RH_APP/Controller/RH_Controller.cs
+++ RH_APP/Controller/RH_Controller.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Mallaca;
+using Newtonsoft.Json;
 namespace RH_APP.Controller
 {
     class RH_Controller
@@ -69,6 +70,14 @@ namespace RH_APP.Controller
                 }
                 writer.Flush();
             }
+
+        }
+
+        private void SendToServer(object sender, EventArgs args)
+        {
+            string json = JsonConvert.SerializeObject(this.LatestMeasurement);
+            Classes.Client.Send(json);
+            Console.Write(json);
 
         }
 
