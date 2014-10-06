97b977d8ae2dd25d3bd862bdd4b511ffc4132bc4
Engin <Engin@Engin.xx>
2014-10-06 10:03:31 +0200

    Xunit gefixt (hoop ik, denk het niet aangezien george het heeft gedaan)

---
+++ Mallaca/Network/NetworkSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mallaca.Network
+{
+    public static class NetworkSettings
+    {
+        public static string ServerIP = "127.0.0.1";
+        public static int ServerPort = 9001;
+
+        public static string ClientIP {
+            get { 
+                var clientip = Dns.GetHostEntry(Dns.GetHostName())
+                .AddressList.First(o => o.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                .ToString();
+                return clientip; 
+            }
+        }
+
+        public static IPEndPoint ServerEndPoint = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
+    }
+}
--- Mallaca/Network/TCPController.cs
+++ Mallaca/Network/TCPController.cs
@@ -1,56 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Tasks;
+using Mallaca.Network;
 
-namespace Mallaca.Network
+namespace RH_APP.Controller
 {
-// ReSharper disable once InconsistentNaming
+    // ReSharper disable once InconsistentNaming
     public class TCPController
     {
-        private static Socket _socket;
+        private static TcpClient client;
+        private static SslStream sslStream;
         public static Boolean Busy { get; private set; }
 
-        public static void StartConnection()
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
-            // Connect to a remote device.
-            try
+            if (sslPolicyErrors == SslPolicyErrors.None)
             {
-                // Establish the remote endpoint for the socket.
-                var remoteEP = new IPEndPoint(IPAddress.Parse("10.0.1.11"), 9001);//145.48.205.97
-
-                // Create a TCP/IP socket.
-                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect to the remote endpoint.
-                _socket.BeginConnect(remoteEP, ConnectCallback, _socket);
-                Console.WriteLine("Client connected...");
-
-
-                // Release the socket.
+                return true;
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("ERROR!! " + e.ToString());
-            }
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers. 
+            return false;
         }
 
-        public static void StopConnection()
+        public static void runClient()
         {
-            if (_socket == null) return;
+            client = new TcpClient();
+            client.BeginConnect(NetworkSettings.ServerIP, NetworkSettings.ServerPort, ConnectCallback, null);
+            Console.WriteLine("Client connected...");
+        }
 
-            _socket.Shutdown(SocketShutdown.Both);
-            _socket.Disconnect(false);
-            _socket = null;
-            Console.WriteLine("Socket closed...");
+        public static void stopClient()
+        {
+            if (client == null) return;
+            client.Close();
+            client = null;
+            Console.WriteLine("Client closed...");
         }
 
         public static void Send(String data)
         {
             //Socket must be set to any instance....
-            if (_socket == null)
+            if (client == null)
             {
-                StartConnection();
+                runClient();
             }
 
             // Convert the string data to byte data using ASCII encoding.
@@ -58,47 +61,62 @@ namespace Mallaca.Network
 
             // Begin sending the data
             Busy = true;
-            _socket.BeginSend(byteData, 0, byteData.Length, 0,SendCallback, _socket);
-            Console.WriteLine("Sent data: " + data);
+            sslStream.BeginWrite(byteData, 0, byteData.Length, SendCallback, sslStream);
+            sslStream.Flush();
+
+            Console.WriteLine("Data sent: " + data);
         }
 
         private static void ConnectCallback(IAsyncResult ar)
         {
+            sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+
             try
             {
-                // Retrieve the socket
-                var socket = (Socket)ar.AsyncState;
-
-                // Complete the connection.
-                socket.EndConnect(ar);
-
-                // Signal that connected
-                Console.WriteLine("TCPController: Connection active");
+                sslStream.AuthenticateAsClient(NetworkSettings.ServerIP);
             }
-            catch (Exception e)
+
+            catch (AuthenticationException e)
             {
-                Console.WriteLine("TCPController ERROR!! \n" + e.ToString());
+                Console.WriteLine("Exception: {0}", e.Message);
+
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+
+                Console.WriteLine("Authentication failed - closing the connection.");
+
+                stopClient();
             }
+            var tcpclient = (TcpClient)ar.AsyncState;
+            // Complete the connection.
+            tcpclient.EndConnect(ar);
+
+            // Signal that connected
+            Console.WriteLine("TCPController: Connection active");
+
         }
 
         private static void SendCallback(IAsyncResult ar)
         {
             try
             {
-                // Retrieve the socket
-                var cl = (Socket)ar.AsyncState;
+                //Retrieve the SslStream
+                var sslstream = (SslStream)ar.AsyncState;
 
-                // Complete sending the data to the remote device.
-                var bytesSent = cl.EndSend(ar);
+                //Complete sending the data to the remote device
+                sslstream.EndWrite(ar);
+
                 Busy = false;
-                Console.WriteLine("Sent to server...", bytesSent);
+                Console.WriteLine("Sent to server...");
 
             }
+
             catch (Exception e)
             {
                 Console.WriteLine("ERROR!!" + e.ToString());
             }
         }
-
     }
 }
