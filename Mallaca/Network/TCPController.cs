be4614bb140485816146816998a2c5b8715af40d
JordySipkema <jordysipkema@gmail.com>
2014-10-06 11:55:50 +0200

    Fix.

--- Mallaca/Network/TCPController.cs
+++ Mallaca/Network/TCPController.cs
@@ -1,79 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
-using Mallaca.Network;
+using System.Threading;
+using Mallaca.Properties;
 
-namespace RH_APP.Controller
+namespace Mallaca.Network
 {
     // ReSharper disable once InconsistentNaming
     public class TCPController
     {
-        private static TcpClient client;
-        private static SslStream sslStream;
+        private static TcpClient _client;
+        private static SslStream _sslStream;
         public static Boolean Busy { get; private set; }
 
-        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        public static void RunClient()
         {
-            if (sslPolicyErrors == SslPolicyErrors.None)
-            {
-                return true;
-            }
+            _client = new TcpClient();
+            _client.Connect(NetworkSettings.ServerIP, NetworkSettings.ServerPort);
 
-            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
-
-            // Do not allow this client to communicate with unauthenticated servers. 
-            return false;
-        }
-
-        public static void runClient()
-        {
-            client = new TcpClient();
-            client.BeginConnect(NetworkSettings.ServerIP, NetworkSettings.ServerPort, ConnectCallback, null);
-            Console.WriteLine("Client connected...");
-        }
-
-        public static void stopClient()
-        {
-            if (client == null) return;
-            client.Close();
-            client = null;
-            Console.WriteLine("Client closed...");
-        }
-
-        public static void Send(String data)
-        {
-            //Socket must be set to any instance....
-            if (client == null)
-            {
-                runClient();
-            }
-
-            // Convert the string data to byte data using ASCII encoding.
-            var byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data
-            Busy = true;
-            sslStream.BeginWrite(byteData, 0, byteData.Length, SendCallback, sslStream);
-            sslStream.Flush();
-
-            Console.WriteLine("Data sent: " + data);
-        }
-
-        private static void ConnectCallback(IAsyncResult ar)
-        {
-            sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+            // ReSharper disable once RedundantDelegateCreation
+            _sslStream = new SslStream(_client.GetStream(), false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
 
             try
             {
-                sslStream.AuthenticateAsClient(NetworkSettings.ServerIP);
+                _sslStream.AuthenticateAsClient(NetworkSettings.ServerIP);
             }
 
             catch (AuthenticationException e)
@@ -87,15 +41,44 @@ namespace RH_APP.Controller
 
                 Console.WriteLine("Authentication failed - closing the connection.");
 
-                stopClient();
+                StopClient();
             }
-            var tcpclient = (TcpClient)ar.AsyncState;
-            // Complete the connection.
-            tcpclient.EndConnect(ar);
 
             // Signal that connected
             Console.WriteLine("TCPController: Connection active");
+        }
 
+        public static void StopClient()
+        {
+            if (_client == null) return;
+            _client.Close();
+            _client = null;
+            Console.WriteLine("Client closed...");
+        }
+
+        public static void Send(String data)
+        {
+            if (_client == null && !Busy)
+            {
+                RunClient();
+            }
+            else if (_client == null && Busy)
+            {
+                while (Busy)
+                {
+                    Thread.Sleep(10);
+                }
+            }
+
+            // Convert the string data to byte data using ASCII encoding.
+            var byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data
+            Busy = true;
+            _sslStream.BeginWrite(byteData, 0, byteData.Length, SendCallback, _sslStream);
+            _sslStream.Flush();
+
+            Console.WriteLine("Data sent: " + data);
         }
 
         private static void SendCallback(IAsyncResult ar)
@@ -113,10 +96,17 @@ namespace RH_APP.Controller
 
             }
 
-            catch (Exception e)
+            catch (Exception exception)
             {
-                Console.WriteLine("ERROR!!" + e.ToString());
+                Console.WriteLine("ERROR!!: " + exception);
             }
+        }
+
+        private static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            var servercert = new X509Certificate2(Resources._23tia5_centificate, "AvansHogeschool23ti2a5");
+            return certificate.Equals(servercert);
         }
     }
 }
