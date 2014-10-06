ac47269baf3f5f1a3ec88e846be5ff3e9e173744
Cave Johnson <Cave@ApertureScience.com>
2014-10-06 12:43:12 +0200

    More packets.

---
+++ Mallaca/Network/Packet/LoginResponsePacket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Mallaca.Network.Packet
+{
+    public class LoginResponsePacket : ResponsePacket
+    {
+        private const string cmd = "RESP-LOGIN";
+        public string authtoken { get; set; }
+
+        public LoginResponsePacket()
+        {
+            
+        }
+
+        public LoginResponsePacket(string status, string desc, string authToken) : base(status, desc)
+        {
+            this.authtoken = authToken;
+        }
+
+        public override string ToString()
+        {
+            JObject json = base.GetJsonObject();
+            json.Add("CMD", cmd);
+            json.Add("authToken", authtoken);
+            return json.ToString();
+        }
+    }
+}
