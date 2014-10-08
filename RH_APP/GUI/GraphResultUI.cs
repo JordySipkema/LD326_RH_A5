5b2558a02b1f25070591d07324af9e7d553a9334
Farid-19 <faridamali93@gmail.com>
2014-10-08 09:54:30 +0200

    Result graph opzet

---
+++ RH_APP/GUI/GraphResultUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mallaca;
+
+namespace RH_APP.GUI
+{
+    public partial class GraphResultUI : Form
+    {
+        private List<Measurement> result;
+
+        public GraphResultUI()
+        {
+            InitializeComponent();
+        }
+
+        public List<Measurement> receiveMeasurements(List<Measurement> origin)
+        {
+
+            return result;
+        }
+
+
+        public void printGraph()
+        {
+
+        }
+    }
+}
