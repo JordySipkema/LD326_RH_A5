9827ab33edb4626a084d55f71fa3493841778ea7
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2014-10-15 14:54:22 +0200

    connect user cancel button bug fix

--- RH_APP/GUI/CreateConnectionScreen.cs
+++ RH_APP/GUI/CreateConnectionScreen.cs
@@ -44,15 +44,6 @@ namespace RH_APP.GUI
             }
         }
 
-        private void CreateConnectionScreen_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            DialogResult dialog = dialog = MessageBox.Show("Are you sure you want to cancel?", "Alert", MessageBoxButtons.YesNo);
-            if (dialog == DialogResult.Yes)
-            {
-                this.Close();
-            }
-        }
-
         private void _clientList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
