5c8599d20ae9b6dbf126b6a1aaad10c0b2c975ba
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2014-10-15 14:44:34 +0200

    reset bug fixed

--- RH_APP/Classes/COM_Bike.cs
+++ RH_APP/Classes/COM_Bike.cs
@@ -23,8 +23,6 @@ namespace RH_APP.Classes
             serial.WriteTimeout = 50;
 
             serial.Open();
-            serial.WriteLine("RS");
-            serial.WriteLine("PT5000");
             serial.WriteLine("CM");
         }
 
