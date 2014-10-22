eb5dac88d4d19b295048a693d548423f239ce0f6
JordySipkema <jordysipkema@gmail.com>
2014-10-22 10:37:05 +0200

    Mallaca>Network>Packet: Add subscribe packet
    Signed-off-by: JordySipkema <jordysipkema@gmail.com>

---
+++ Mallaca/Network/Packet/Request/SubscribePacket.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Mallaca.Network.Packet.Request
+{
+    public class SubscribePacket : AuthenticatedPacket
+    {
+        //Inherited field: CMD, AuthToken
+        //Introduced fields: Client, Subscribe (bool)
+
+        //Note: Subscribe is true for subscribing, false for unsubscribing.
+
+        public string Client { get; private set; }
+        public bool Subscribe { get; private set; }
+
+        public SubscribePacket(string usernameClient, bool subscribe, string cmd, string authtoken) : base(cmd, authtoken)
+        {
+            Initialize(usernameClient, subscribe);
+        }
+
+        private void Initialize(string usernameClient, bool subscribe)
+        {
+            Client = usernameClient;
+            Subscribe = subscribe;
+        }
+
+        public override JObject ToJsonObject()
+        {
+            var json = base.ToJsonObject();
+            json.Add("Client", Client);
+            json.Add("Subscribe", Subscribe);
+            return json;
+        }
+
+        public override string ToString()
+        {
+            return ToJsonObject().ToString();
+        }
+    }
+}
